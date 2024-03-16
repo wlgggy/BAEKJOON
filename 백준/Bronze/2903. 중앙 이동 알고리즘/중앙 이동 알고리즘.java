83eb7d62de7bd7a295f266816626c4a9ccf3f81c
wlgggy <129392653+wlgggy@users.noreply.github.com>
2024-03-16 20:53:44 +0900

    [Bronze III] Title: 중앙 이동 알고리즘, Time: 204 ms, Memory: 17776 KB -BaekjoonHub

---
+++ 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt(); 
+        
+        int result = (int) Math.pow((int) Math.pow(2, n) + 1, 2);
+
+        System.out.println(result);
+    }
+}
