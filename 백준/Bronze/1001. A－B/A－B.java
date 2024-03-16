9567fefb8f629a76d81553bf5ddb3bc7a27c9362
wlgggy <129392653+wlgggy@users.noreply.github.com>
2024-03-16 18:39:16 +0900

    [Bronze V] Title: A-B, Time: 208 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+
+        int result = A - B;
+
+        System.out.println(result);
+
+    }
+}
