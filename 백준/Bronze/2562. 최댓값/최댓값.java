53195604be9215d4c2edd9a79e875e970df30cc7
wlgggy <129392653+wlgggy@users.noreply.github.com>
2024-03-16 19:36:02 +0900

    [Bronze III] Title: 최댓값, Time: 208 ms, Memory: 17672 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int max = 0; 
+        int maxIndex = 0; 
+
+        for (int i = 1; i <= 9; i++) {
+            int num = scanner.nextInt();
+
+            if (num > max) {
+                max = num;
+                maxIndex = i;
+            }
+        }
+
+        System.out.println(max);
+
+        System.out.println(maxIndex);
+    }
+}
