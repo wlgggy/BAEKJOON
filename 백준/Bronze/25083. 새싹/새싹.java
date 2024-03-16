143d997ec0c02ef34f4baeefe13dd8655ae45160
wlgggy <129392653+wlgggy@users.noreply.github.com>
2024-03-16 22:08:40 +0900

    [Bronze V] Title: 새싹, Time: 116 ms, Memory: 13940 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+            printSprout();
+        }
+
+        private static void printSprout() {
+            System.out.println("         ,r'\"7");
+            System.out.println("r`-_   ,'  ,/");
+            System.out.println(" \\. \". L_r'");
+            System.out.println("   `~\\/");
+            System.out.println("      |");
+            System.out.println("      |");
+
+    }
+}
