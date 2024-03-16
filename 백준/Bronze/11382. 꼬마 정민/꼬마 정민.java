b77674b3a3b8dc883c73f226eb89eead3a69e48b
wlgggy <129392653+wlgggy@users.noreply.github.com>
2024-03-16 22:06:13 +0900

    [Bronze V] Title: 꼬마 정민, Time: 204 ms, Memory: 17676 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        long A = scanner.nextLong();
+        long B = scanner.nextLong();
+        long C = scanner.nextLong();
+
+        long result = A + B + C;
+        System.out.println(result);
+
+    }
+}
