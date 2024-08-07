315808308883a7a11f4a956b1e909c75ab36eef5
wlgggy <129392653+wlgggy@users.noreply.github.com>
2024-08-07 23:57:43 +0900

    [Bronze III] Title: 그대로 출력하기 2, Time: 184 ms, Memory: 17944 KB -BaekjoonHub

---
+++ 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            System.out.println(line);
+        }
+    }
+}
